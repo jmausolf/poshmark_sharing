0c255c69c2e7c95e7cce27ea4b4ead5b9e31562e
jmausolf <jgm346@gmail.com>
2017-06-04 17:24:03 -0500

    adding example credentials file

---
+++ example_credentials.py
@@ -0,0 +1,6 @@
+#Enter Your Credentials Here
+#amazon_email = "myemail@gmail.com"
+#amazon_password = "mysupersecret"
+
+#Rename this file "credentials.py"
+#mv example_credentials.py credentials.py

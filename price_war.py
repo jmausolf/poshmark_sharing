3de309bb7334ab01141e3cdf5f3ac010e5a79de4
jmausolf <jgm346@gmail.com>
2017-06-04 10:57:55 -0500

    working on finding price field and updating it

--- price_war.py
+++ price_war.py
@@ -7,26 +7,24 @@ from selenium.webdriver.common.keys import Keys
 #Your Amazon Credentials File
 from credentials import *
 
+#Define URL and Web Driver
 url = "https://sellercentral.amazon.com/inventory/ref=id_invmgr_dnav_xx_?tbla_myitable=sort:%7B%22sortOrder%22%3A%22DESCENDING%22%2C%22sortedColumnId%22%3A%22date%22%7D;search:;pagination:1;"
-
 driver = webdriver.Firefox()
 driver.get(url)
 
-#assert "NatureServe" in driver.title
-#SciName = driver.find_element_by_name('searchSciOrCommonName')
-
+#Login
 username = driver.find_element_by_name("email")
 username.send_keys(amazon_email)
+time.sleep(5)
 
 password = driver.find_element_by_name("password")
-time.sleep(5)
 password.send_keys(amazon_password)
 time.sleep(5)
+
 password.send_keys(Keys.RETURN)
-
-
 driver.implicitly_wait(10)
 
+#Price Match Inventory
 def match_prices():
 
     price_match = driver.find_elements_by_link_text("Match price")
@@ -41,7 +39,20 @@ def match_prices():
 
 
 
+def price_war(price):
+    new_price = round(price - 0.01, 2)
+    return new_price
+
+
+item_price = driver.find_element_by_name("price")
+item_price.send_keys(109.75)
 
 print(len(driver.find_elements_by_xpath('//a')))
 
 #match_prices(5)
+
+item_price = driver.find_element_by_class_name("a-input-text main-entry mt-icon-input mt-input-text")
+item_price.send_keys(109.75)
+
+a-input-text main-entry mt-icon-input mt-input-text
+a-input-text main-entry mt-input-text

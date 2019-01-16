187e2341f9d3e18b2964a332a60727df5186d050
Joshua Gary Mausolf <jmausolf@users.noreply.github.com>
2019-01-16 01:00:36 -0600

    Update example_credentials.py

--- example_credentials.py
+++ example_credentials.py
@@ -1,6 +1,6 @@
 #Enter Your Credentials Here
-#poshmark_username = "myAccountName"
-#poshmark_password = "mysupersecret"
+#poshmark_username = "poshmarkusername"
+#poshmark_password = "poshmarkpassword"
 
 #Rename this file "credentials.py"
 #mv example_credentials.py credentials.py
